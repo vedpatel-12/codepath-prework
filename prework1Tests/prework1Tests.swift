553998dd89ed0ef31f2a3893f16c559c6de866be
vedpatel-12 <vedptl@gmail.com>
2025-01-22 17:22:53 -0500

    added prework files to repository

---
+++ prework1Tests/prework1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  prework1Tests.swift
+//  prework1Tests
+//
+//  Created by Ved Patel on 1/22/25.
+//
+
+import Testing
+@testable import prework1
+
+struct prework1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
